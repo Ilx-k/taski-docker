bb5304d54be267d38a74f694b2997d6d0e32d940
Стас Басов <stas.basov@whatever.com>
2024-02-11 19:59:29 +0300

    pup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'taski'),
         'USER': os.getenv('POSTGRES_USER', 'taski'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
