bb883758d2f174b1c5bec27fac78478fbf1c9d02
Стас Басов <stas.basov@whatever.com>
2024-02-11 20:19:17 +0300

    back

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,11 +62,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
+        # Меняем настройку Django: теперь для работы будет использоваться
+        # бэкенд postgresql
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'taski'),
-        'USER': os.getenv('POSTGRES_USER', 'taski'),
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
