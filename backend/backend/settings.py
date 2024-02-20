825610932d38b1ff6f7822600a4a9d2e633f1b4b
Стас Басов <stas.basov@whatever.com>
2024-02-20 16:35:13 +0300

    last

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['158.160.74.43', '127.0.0.1', 'locallhost', 'piratskayagavan.myftp.org']
 
 
 # Application definition
